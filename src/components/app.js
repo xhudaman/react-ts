085b17a398393d02c13d49a159c7646063b971b9
Matthew Edell <edell.matthew@gmail.com>
2022-08-12 14:50:55 -0400

    Added css to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>I gave up on CSS...</p>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/app";
+import "./index.css";
 
 ReactDom.render(<App />, document.getElementById("root"));
