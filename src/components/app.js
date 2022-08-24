f32c5d584c3d3cc71a02f572a382391a4d213c27
Matthew Edell <edell.matthew@gmail.com>
2022-08-24 13:11:26 -0400

    Configured react to not need import of React

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,8 @@
-import React from "react";
-
 const App = () => {
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <h1 className="text-2xl">Hello world!</h1>
-      <p className="text-red-500">I gave up on CSS...</p>
+      <p className="text-green-500">I gave up on CSS...</p>
     </div>
   );
 };
